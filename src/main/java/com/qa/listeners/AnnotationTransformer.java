3bcff6238ccdcb7a608ca0356b19bf595311d356
ronitroy <ronit@100ms.live>
2022-07-07 03:38:56 +0530

    retry listener

---
+++ src/main/java/com/qa/listeners/AnnotationTransformer.java
@@ -0,0 +1,37 @@
+package com.qa.listeners;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class AnnotationTransformer implements IAnnotationTransformer {
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+        annotation.setRetryAnalyzer(Retry.class);
+    }
+
+    public class Retry implements IRetryAnalyzer {
+
+        private int count = 0;
+        private int maxTry = 3;
+
+        @Override
+        public boolean retry(ITestResult iTestResult) {
+            if (!iTestResult.isSuccess()) {                      //Check if test not succeed
+                if (count < maxTry) {                            //Check if maxtry count is reached
+                    count++;                                     //Increase the maxTry count by 1
+                    iTestResult.setStatus(ITestResult.FAILURE);  //Mark test as failed
+                    return true;                                 //Tells TestNG to re-run the test
+                } else {
+                    iTestResult.setStatus(ITestResult.FAILURE);  //If maxCount reached,test marked as failed
+                }
+            } else {
+                iTestResult.setStatus(ITestResult.SUCCESS);      //If test passes, TestNG marks it as passed
+            }
+            return false;
+        }
+}}
